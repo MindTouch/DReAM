c34604f98b637af5ce826f587d6bdbd3db82539d
Arne Claassen <arne@claassen.net>
2011-02-15 11:01:25 -0800

    added a test for observing timer accuracy

--- src/tests/DreamMisc/TaskTimerTests.cs
+++ src/tests/DreamMisc/TaskTimerTests.cs
@@ -19,11 +19,14 @@
  * limitations under the License.
  */
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using MindTouch;
 using MindTouch.Dream;
 using MindTouch.Tasking;
 using NUnit.Framework;
+using System.Linq;
 
 namespace MindTouch.Dream.Test {
 
@@ -31,7 +34,35 @@ namespace MindTouch.Dream.Test {
     public class TaskTimerTests {
 
         private static readonly log4net.ILog _log = LogUtils.CreateLog();
-        
+
+        [TestFixtureSetUp]
+        public void GlobalSetup() {
+
+            // Note (arnec): need to prime threadpool
+            var q = Async.GlobalDispatchQueue;
+        }
+
+        [Test, Ignore("timing test to be visually observed")]
+        public void Determine_tasktimer_accuracy() {
+            var r = new Random();
+            var deltas = new List<int>();
+            for(var i = 0; i < 100; i++) {
+                var interval = r.Next(100, 600);
+                var stopwatch = Stopwatch.StartNew();
+                TaskTimerFactory.Current.New(TimeSpan.FromMilliseconds(interval), tt => {
+                    stopwatch.Stop();
+                    var delta = stopwatch.ElapsedMilliseconds - interval;
+                    double avg = 0;
+                    lock(deltas) {
+                        deltas.Add((int)delta);
+                        avg = deltas.Average();
+                    }
+                    Console.WriteLine("Expected {0:0}ms was {1:0}ms, delta {2:0}ms, avg {3:0}", interval, stopwatch.ElapsedMilliseconds, delta, avg);
+                }, null, TaskEnv.None);
+                Thread.Sleep(300);
+            }
+        }
+
         [Test]
         public void TaskTimer_Shutdown() {
             var shouldFire = new ManualResetEvent(false);
@@ -53,7 +84,7 @@ namespace MindTouch.Dream.Test {
                 null,
                 TaskEnv.None);
             _log.DebugFormat("waiting for first task");
-            Assert.IsTrue(shouldFire.WaitOne(2000,false));
+            Assert.IsTrue(shouldFire.WaitOne(2000, false));
             _log.DebugFormat("starting shutdown");
             TaskTimer.Shutdown();
             _log.DebugFormat("shutdown complete");
