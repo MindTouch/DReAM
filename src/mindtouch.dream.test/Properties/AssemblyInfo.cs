82e859923c46550d3e2634a27c9bb879fa4d83bb
Arne Claassen <arne@claassen.net>
2011-02-28 16:37:06 -0800

    updating version information to match new build process

--- src/mindtouch.dream.test/Properties/AssemblyInfo.cs
+++ src/mindtouch.dream.test/Properties/AssemblyInfo.cs
@@ -53,11 +53,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.0.1")]
-[assembly: AssemblyVersion("2.3.0.1")]
-[assembly: AssemblyFileVersion("2.3.0.1")]
+// [assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 [assembly: GitBranch("master")]
-[assembly: GitRevision("fa60bcf055b176bc225247412941c0225097279a")]
+[assembly: GitRevision("HEAD")]
 [assembly: GitUri("https://github.com/MindTouch/DReAM")]
 
 #if SIGNED
