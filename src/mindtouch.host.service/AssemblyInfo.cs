dec225c8884f3cc5dd4c7ea48f79edea2dac3895
Arne Claassen <arne@claassen.net>
2011-07-07 22:47:52 -0700

    incremented release to 2.2.4

--- src/mindtouch.host.service/AssemblyInfo.cs
+++ src/mindtouch.host.service/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
+[assembly: AssemblyVersion("2.2.4.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
 [assembly: GitBranch("2.2")]
 [assembly: GitRevision("HEAD")]
 [assembly: GitUri("https://github.com/MindTouch/DReAM")]
