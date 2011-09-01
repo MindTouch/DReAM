1d98037f8284eb720c6a5ad81ccb082ee770679b
Arne Claassen <arne@claassen.net>
2011-09-01 16:12:22 -0700

    updating version of master to 2.4

--- src/mindtouch.host/AssemblyInfo.cs
+++ src/mindtouch.host/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
-[assembly: GitBranch("2.3")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: GitBranch("master")]
 [assembly: GitRevision("HEAD")]
 [assembly: GitUri("https://github.com/MindTouch/DReAM")]
