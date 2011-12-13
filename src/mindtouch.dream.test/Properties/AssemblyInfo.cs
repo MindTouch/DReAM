e9ecde9b60b4acdcda7349956d7cd0b5a73e661b
Arne Claassen <arne@claassen.net>
2011-12-13 11:00:02 -0800

    incrementing release to 2.3.2.0

--- src/mindtouch.dream.test/Properties/AssemblyInfo.cs
+++ src/mindtouch.dream.test/Properties/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyVersion("2.3.2.0")]
+[assembly: AssemblyFileVersion("2.3.2.0")]
 [assembly: GitBranch("2.3")]
 [assembly: GitRevision("HEAD")]
 [assembly: GitUri("https://github.com/MindTouch/DReAM")]
