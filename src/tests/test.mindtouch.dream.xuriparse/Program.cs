aa451a51a2c05026cdfca47589c2c1831b273e38
Steve Bjorg <steve.bjorg@gmail.com>
2014-01-30 08:16:25 -0800

    Minor changes to the test app.

--- src/tests/test.mindtouch.dream.xuriparse/Program.cs
+++ src/tests/test.mindtouch.dream.xuriparse/Program.cs
@@ -83,141 +83,152 @@ namespace MindTouchTest.Dream {
                 Console.WriteLine();
             }
 
-            // TODO (steveb): compare results produced by XUri and XUriParser
+            // compare results produced by XUri and XUriParser
             var passed = 0;
             var skipped = 0;
-            failed = 0;
-            foreach(var uri in uris) {
-                string schemeRegex;
-                string userRegex;
-                string passwordRegex;
-                string hostnameRegex;
-                int portRegex;
-                bool usesDefaultPortRegex;
-                bool trailingSlashRegex;
-                string[] segmentsRegex;
-                KeyValuePair<string, string>[] paramsRegex;
-                string fragmentRegex;
-                var successRegex = XUri.TryParse(uri, out schemeRegex, out userRegex, out passwordRegex, out hostnameRegex, out portRegex, out usesDefaultPortRegex, out segmentsRegex, out trailingSlashRegex, out paramsRegex, out fragmentRegex);
+            Timer("compare results of XUri.TryParse() and XUriParser.TryParse()", () => {
+                failed = 0;
+                foreach(var uri in uris) {
+                    string schemeRegex;
+                    string userRegex;
+                    string passwordRegex;
+                    string hostnameRegex;
+                    int portRegex;
+                    bool usesDefaultPortRegex;
+                    bool trailingSlashRegex;
+                    string[] segmentsRegex;
+                    KeyValuePair<string, string>[] paramsRegex;
+                    string fragmentRegex;
+                    var successRegex = XUri.TryParse(uri, out schemeRegex, out userRegex, out passwordRegex, out hostnameRegex, out portRegex, out usesDefaultPortRegex, out segmentsRegex, out trailingSlashRegex, out paramsRegex, out fragmentRegex);
 
-                string schemeCustom;
-                string userCustom;
-                string passwordCustom;
-                string hostnameCustom;
-                int portCustom;
-                bool usesDefaultPortCustom;
-                bool trailingSlashCustom;
-                string[] segmentsCustom;
-                KeyValuePair<string, string>[] paramsCustom;
-                string fragmentCustom;
-                var successCustom = XUriParser.TryParse(uri, out schemeCustom, out userCustom, out passwordCustom, out hostnameCustom, out portCustom, out usesDefaultPortCustom, out segmentsCustom, out trailingSlashCustom, out paramsCustom, out fragmentCustom);
+                    string schemeCustom;
+                    string userCustom;
+                    string passwordCustom;
+                    string hostnameCustom;
+                    int portCustom;
+                    bool usesDefaultPortCustom;
+                    bool trailingSlashCustom;
+                    string[] segmentsCustom;
+                    KeyValuePair<string, string>[] paramsCustom;
+                    string fragmentCustom;
+                    var successCustom = XUriParser.TryParse(uri, out schemeCustom, out userCustom, out passwordCustom, out hostnameCustom, out portCustom, out usesDefaultPortCustom, out segmentsCustom, out trailingSlashCustom, out paramsCustom, out fragmentCustom);
 
-                if(successRegex != successCustom) {
-                    Console.WriteLine("FAILED TryParse: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(!successRegex) {
+                    if(successRegex != successCustom) {
+                        Console.WriteLine("FAILED TryParse: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(!successRegex) {
 
-                    // if parsing failed, no point in comparing the outcome of the out arguments since the behavior does not need to be the same
-                    continue;
-                }
-                if(schemeRegex != schemeCustom) {
-                    Console.WriteLine("FAILED scheme: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(userRegex != userCustom) {
-                    Console.WriteLine("FAILED user: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(passwordRegex != passwordCustom) {
-                    Console.WriteLine("FAILED password: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(portRegex != portCustom) {
-                    Console.WriteLine("FAILED port: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(usesDefaultPortRegex != usesDefaultPortCustom) {
-                    Console.WriteLine("FAILED usesDefaultPort: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(trailingSlashRegex != trailingSlashCustom) {
-                    Console.WriteLine("FAILED trailingSlash: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if((segmentsRegex == null && segmentsCustom != null) || (segmentsRegex != null && segmentsCustom == null)) {
-                    Console.WriteLine("FAILED segments: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(segmentsRegex != null) {
-                    if(segmentsRegex.Length != segmentsCustom.Length) {
+                        // if parsing failed, no point in comparing the outcome of the out arguments since the behavior does not need to be the same
+                        continue;
+                    }
+                    if(schemeRegex != schemeCustom) {
+                        Console.WriteLine("FAILED scheme: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(userRegex != userCustom) {
+                        Console.WriteLine("FAILED user: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(passwordRegex != passwordCustom) {
+                        Console.WriteLine("FAILED password: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(portRegex != portCustom) {
+                        Console.WriteLine("FAILED port: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(usesDefaultPortRegex != usesDefaultPortCustom) {
+                        Console.WriteLine("FAILED usesDefaultPort: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if(trailingSlashRegex != trailingSlashCustom) {
+                        Console.WriteLine("FAILED trailingSlash: {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    if((segmentsRegex == null && segmentsCustom != null) || (segmentsRegex != null && segmentsCustom == null)) {
                         Console.WriteLine("FAILED segments: {0}", uri);
                         ++failed;
                         continue;
                     }
-                    for(var i = 0; i < segmentsRegex.Length; ++i) {
-                        if(segmentsRegex[i] != segmentsCustom[i]) {
-                            if(segmentsCustom[i].Replace('\\', '/') != segmentsRegex[i]) {
-                                Console.WriteLine("FAILED : {0}", uri);
-                                ++failed;
-                                goto skip;
-                            }
+                    if(segmentsRegex != null) {
+                        if(segmentsRegex.Length != segmentsCustom.Length) {
+                            Console.WriteLine("FAILED segments: {0}", uri);
+                            ++failed;
+                            continue;
+                        }
+                        for(var i = 0; i < segmentsRegex.Length; ++i) {
+                            if(segmentsRegex[i] != segmentsCustom[i]) {
+                                if(segmentsCustom[i].Replace('\\', '/') != segmentsRegex[i]) {
+                                    Console.WriteLine("FAILED : {0}", uri);
+                                    ++failed;
+                                    goto skip;
+                                }
 
-                            // regex-parser does not preserve '\' in original uri
-                            ++skipped;
+                                // regex-parser does not preserve '\' in original uri
+                                ++skipped;
+                            }
                         }
                     }
-                }
-                if((paramsRegex == null && paramsCustom != null) || (paramsRegex != null && paramsCustom == null)) {
-                    Console.WriteLine("FAILED params: {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                if(paramsRegex != null) {
-                    if(paramsRegex.Length != paramsCustom.Length) {
+                    if((paramsRegex == null && paramsCustom != null) || (paramsRegex != null && paramsCustom == null)) {
                         Console.WriteLine("FAILED params: {0}", uri);
                         ++failed;
                         continue;
                     }
-                    for(var i = 0; i < paramsRegex.Length; ++i) {
-                        if(paramsRegex[i].Key != paramsCustom[i].Key) {
-                            Console.WriteLine("FAILED : {0}", uri);
+                    if(paramsRegex != null) {
+                        if(paramsRegex.Length != paramsCustom.Length) {
+                            Console.WriteLine("FAILED params: {0}", uri);
                             ++failed;
-                            goto skip;
+                            continue;
                         }
-                        if(paramsRegex[i].Value != paramsCustom[i].Value) {
-                            Console.WriteLine("FAILED : {0}", uri);
+                        for(var i = 0; i < paramsRegex.Length; ++i) {
+                            if(paramsRegex[i].Key != paramsCustom[i].Key) {
+                                Console.WriteLine("FAILED : {0}", uri);
+                                ++failed;
+                                goto skip;
+                            }
+                            if(paramsRegex[i].Value != paramsCustom[i].Value) {
+                                Console.WriteLine("FAILED : {0}", uri);
+                                ++failed;
+                                goto skip;
+                            }
+                        }
+                    }
+                    if(fragmentRegex != fragmentCustom) {
+                        Console.WriteLine("FAILED : {0}", uri);
+                        ++failed;
+                        continue;
+                    }
+                    ++passed;
+                skip:
+                    var x = 0;
+                }
+            });
+            Console.WriteLine("{0:#,##0} uris passed comparison between regex and custom parser; {1:#,##0} were skipped; and {2:#,##0} failed", passed, skipped, failed);
+
+            // check if URI can be reproduced validly
+            Timer("compare original URI to XUriParser.TryParse().ToString()", () => {
+                failed = 0;
+                foreach(var uri in uris) {
+                    var u = XUriParser.TryParse(uri);
+                    if(u != null) {
+                        var rendered = u.ToString();
+                        if(!rendered.EqualsInvariant(uri)) {
+                            //Console.WriteLine("{0} -> {1}", uri, rendered);
                             ++failed;
-                            goto skip;
                         }
                     }
                 }
-                if(fragmentRegex != fragmentCustom) {
-                    Console.WriteLine("FAILED : {0}", uri);
-                    ++failed;
-                    continue;
-                }
-                ++passed;
-            skip:
-                var x = 0;
-            }
-            Console.WriteLine("{0:#,##0} uris passed comparison between regex and custom parser; {1:#,##0} were skipped; and {2:#,##0} failed", passed, skipped, failed);
-
-            // TODO (steveb): check if URI can be reproduced validly
-            return;
-            foreach(var uri in uris) {
-                var u = XUriParser.TryParse(uri);
-                if((u != null) && !u.ToString().EqualsInvariant(uri)) {
-                    Console.WriteLine("{0} -> {1}", uri, u);
-                }
+            });
+            if(failed > 0) {
+                Console.WriteLine("{0:#,##0} uris failed to render identically to their original", failed);
             }
         }
 
