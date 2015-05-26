ef7cde22112ae5bb68734427e20705c34e776335
Steve Bjorg <steve.bjorg@gmail.com>
2015-05-26 15:23:20 -0700

    Added 'Signed Release' build option for mindtouch.sqs.

--- src/mindtouch.sqs/Properties/AssemblyInfo.cs
+++ src/mindtouch.sqs/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@
  * limitations under the License.
  */
 
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -52,5 +53,38 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: GitBranch("2.5")]
+[assembly: GitRevision("HEAD")]
+[assembly: GitUri("https://github.com/MindTouch/DReAM")]
+
+//
+// In order to sign your assembly you must specify a key to use. Refer to the 
+// Microsoft .NET Framework documentation for more information on assembly signing.
+//
+// Use the attributes below to control which key is used for signing. 
+//
+// Notes: 
+//   (*) If no key is specified, the assembly is not signed.
+//   (*) KeyName refers to a key that has been installed in the Crypto Service
+//       Provider (CSP) on your machine. KeyFile refers to a file which contains
+//       a key.
+//   (*) If the KeyFile and the KeyName values are both specified, the 
+//       following processing occurs:
+//       (1) If the KeyName can be found in the CSP, that key is used.
+//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
+//           in the KeyFile is installed into the CSP and used.
+//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
+//       When specifying the KeyFile, the location of the KeyFile should be
+//       relative to the project output directory which is
+//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
+//       located in the project directory, you would specify the AssemblyKeyFile 
+//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
+//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
+//       documentation for more information on this.
+//
+#if SIGNED
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile(@"../mindtouch.snk")]
+#endif
